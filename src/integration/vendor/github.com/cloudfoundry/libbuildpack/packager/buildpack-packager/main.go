9912fe840af2889249a73fc5046c177be723a5b3
Dave Goddard <dave@goddard.id.au>
2017-07-29 07:53:32 -0400

    Convert to https://github.com/golang/dep

---
+++ src/integration/vendor/github.com/cloudfoundry/libbuildpack/packager/buildpack-packager/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/cloudfoundry/libbuildpack/packager"
+)
+
+func main() {
+	var cached bool
+	var cacheDir string
+
+	version, err := ioutil.ReadFile("VERSION")
+	if err != nil {
+		log.Fatalf("error: Could not read VERSION file: %v", err)
+	}
+
+	flag.BoolVar(&cached, "cached", false, "include dependencies")
+	flag.StringVar(&cacheDir, "cachedir", packager.CacheDir, "cache dir")
+	flag.Parse()
+
+	zipFile, err := packager.Package(".", cacheDir, string(version), cached)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+
+	buildpackType := "uncached"
+	if cached {
+		buildpackType = "cached"
+	}
+
+	stat, err := os.Stat(zipFile)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+
+	fmt.Printf("%s buildpack created and saved as %s with a size of %dMB\n", buildpackType, zipFile, stat.Size()/1024/1024)
+}
