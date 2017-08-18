bdc46c5908f189d80127e2d4cf5bc3ee97a88b26
Forest Eckhardt <forest.eckhardt@gmail.com>
2017-08-18 17:25:03 -0400

    WIP: tests not passing; headers not adding
    Signed-off-by: Keaty Gross <kgross@pivotal.io>

--- src/staticfile/integration/deploy_app_with_hsts_test.go
+++ src/staticfile/integration/deploy_app_with_hsts_test.go
@@ -22,11 +22,13 @@ var _ = Describe("deploy an app using hsts", func() {
 		app = cutlass.New(filepath.Join(bpDir, "fixtures", "with_hsts"))
 	})
 
-	It("provides the Strict-Transport-Security header", func() {
+	FIt("provides the Strict-Transport-Security header", func() {
 		PushAppAndConfirm(app)
 
 		_, headers, err := app.Get("/", map[string]string{})
 		Expect(err).To(BeNil())
 		Expect(headers).To(HaveKey("Strict-Transport-Security"))
+		Expect(headers).To(HaveKey("includeSubDomains"))
+		Expect(headers).To(HaveKey("preload"))
 	})
 })
